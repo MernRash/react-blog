235d40c1c74cef8a600e1d72023ee9e43ca04777
Rahul Shrestha <88819251+MernRash@users.noreply.github.com>
2021-12-14 10:17:11 +0530

    Update more.js

--- src/Components/utility/more.js
+++ src/Components/utility/more.js
@@ -3,7 +3,7 @@ import Latest_Img from '../images/latest-article.jpg'
 import Author from "../Author/author";
 import './style.css';
 
-function More(){
+function More(props){
 
     return(
         <div className="more-card-container">
@@ -12,22 +12,22 @@ function More(){
             <div className="card-container">
             <div>
                 <span>Also tagged ReactJS</span>
-                <img src={Latest_Img} alt="blog_img"></img>
-                <h2>Joshua Tree Overnight Adventure</h2>
-                <Author />
+                <img src={props.imgUrl} alt="blog_img"></img>
+                <h2>{props.title}</h2>
+                <Author name = {props.author} />
             </div>
-            <div>
-                <span>Related Reads</span>
-                <img src={Latest_Img} alt="blog_img"></img>
-                <h2>Joshua Tree Overnight Adventure</h2>
-                <Author />
-            </div>
-            <div>
-                <span>Related Reads</span>
-                <img src={Latest_Img} alt="blog_img"></img>
-                <h2>Joshua Tree Overnight Adventure</h2>
-                <Author />
-            </div>
+//             <div>
+//                 <span>Related Reads</span>
+//                 <img src={Latest_Img} alt="blog_img"></img>
+//                 <h2>Joshua Tree Overnight Adventure</h2>
+//                 <Author />
+//             </div>
+//             <div>
+//                 <span>Related Reads</span>
+//                 <img src={Latest_Img} alt="blog_img"></img>
+//                 <h2>Joshua Tree Overnight Adventure</h2>
+//                 <Author />
+//             </div>
             </div>
         </div>
     )
